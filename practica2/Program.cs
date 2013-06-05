f671721e49736862c1e5d1de7872c2a1c062ff35
GerardoBarragan <gera_shark11@hotmail.com>
2013-06-05 09:28:12 -0500

    practica2

---
+++ practica2/Program.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace RelojCronometro
+{
+	public class Cronometro
+	{
+		public Cronometro ()
+		{
+		}
+		
+		public void inicializar ()
+		{
+			int horas = 0, minutos = 0, segundos = 0;
+			Console.WriteLine ("Cronometro inicializado");
+			
+			for (horas=0; horas<24; horas++) {
+				for (minutos=0; minutos<60; minutos++) {
+					for (segundos=0; segundos<60; segundos++) {
+						Console.WriteLine ("{0}:{1}:{2}", horas, minutos, segundos);
+						System.Threading.Thread.Sleep (1000);
+						Console.Clear ();
+						
+					}
+				}
+			}
+		}
+	}
+}
